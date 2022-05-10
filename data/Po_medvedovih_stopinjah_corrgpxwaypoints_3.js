6cd44b5b0ab6cc23f6d3756b44c7a1e4fa278bc9
ursarotar99 <105304555+ursarotar99@users.noreply.github.com>
2022-05-10 18:38:52 +0200

    Spletna karta_ROTAR
    Spletna karta prikazuje dve pohodniške poti v Sloveniji, in sicer:
    - Planinsko pot Dolenja vas - Veliki Javornik in
    - Pot po Medvedovih stopinjah.
    
    Obe poti se nahajata območju Notranjskega regijskega parka. 
    Podatki so dostopni na spletni povezavi: https://www.notranjski-park.si/obiskovanje/aktivnosti/pohodnistvo (dostopno 10.5.2022).

---
+++ data/Po_medvedovih_stopinjah_corrgpxwaypoints_3.js
@@ -0,0 +1,1 @@
+var json_Po_medvedovih_stopinjah_corrgpxwaypoints_3 = {"type":"FeatureCollection","name":"Po_medvedovih_stopinjah_corrgpxwaypoints_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
